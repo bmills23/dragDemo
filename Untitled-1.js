d4de2576a7472de52284944c8c435a0fb31d8399
bmills23 <58231400+bmills23@users.noreply.github.com>
2023-01-11 16:08:49 -0700

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -14,37 +14,90 @@ Here is an example of how you might achieve this:*/
 //     draggableImage.style.top = (event.clientY - initialPosition.y) + 'px';
 // });
 
-const image = document.getElementById("resizable-image");
+const image = document.getElementById("draggable-resizable-image");
+const dropdown = document.getElementById("dropdown");
+const resizeButton = document.getElementById("resize-button");
+const moveButton = document.getElementById("move-button");
+
+//Resizing
 let resizing = false;
 let aspectRatio;
 let initialX;
 let initialY;
 let initialWidth;
 let initialHeight;
-let threshold = 10;
+
+//Dragging
+let draggable = false;
+let offsetX;
+let offsetY;
+let isSnappedLeft = false;
+let isSnappedRight = false;
 
 image.addEventListener("mousedown", event => {
+    // Show dropdown menu
+    dropdown.classList.remove("hidden");
+    dropdown.style.top = event.clientY + "px";
+    dropdown.style.left = event.clientX + "px";
+
+    // Add event listeners to the buttons
+    resizeButton.addEventListener("click", handleResize);
+    moveButton.addEventListener("click", handleMove);
+});
+
+function handleResize() {
+    // Set up resizing variables
     resizing = true;
     aspectRatio = image.clientWidth / image.clientHeight;
     initialX = event.clientX;
     initialY = event.clientY;
     initialWidth = image.clientWidth;
     initialHeight = image.clientHeight;
-});
+    dropdown.classList.add("hidden");
+}
+
+
+function handleMove() {
+    // Set up draggable variables
+    draggable = true;
+    offsetX = event.clientX - image.offsetLeft;
+    offsetY = event.clientY - image.offsetTop;
+    dropdown.classList.add("hidden");
+}
+
+let leftThreshold = 50; 
+let rightThreshold = 50;
 
 image.addEventListener("mousemove", event => {
-    if (!resizing) {
-        return;
+    if (resizing) {
+        let newWidth = aspectRatio * (event.clientY - initialY + initialHeight);
+        image.style.width = newWidth + "px";
+        image.style.height = (event.clientY - initialY + initialHeight) + "px";
     }
-    if (Math.abs(event.clientX - initialX) < threshold && Math.abs(event.clientY - initialY) < threshold) {
-        return;
+    if (draggable) {
+        let left = event.clientX - offsetX;
+        let top = event.clientY - offsetY;
+        image.style.left = left + "px";
+        image.style.top = top + "px";
+        if (event.clientX < editableRect.left + (editableRect.width / 2) && !isSnappedLeft && Math.abs(event.clientX - offsetX) > leftThreshold) {
+            image.style.cssFloat = "left";
+            isSnappedLeft = true;
+            isSnappedRight = false;
+        } else if (event.clientX >= editableRect.left + (editableRect.width / 2) && !isSnappedRight && Math.abs(event.clientX - offsetX) > rightThreshold) {
+            image.style.cssFloat = "right";
+            isSnappedLeft = false;
+            isSnappedRight = true;
+        }
     }
-    let newWidth = aspectRatio * (event.clientY - initialY + initialHeight);
-    image.style.width = newWidth + "px";
-    image.style.height = (event.clientY - initialY + initialHeight) + "px";
 });
 
-document.addEventListener("mouseup", () => {
+document.addEventListener("mouseup", (event) => {
+      // check whether the image is more than half on the left or right
+      if (event.clientX < (editableRect.left + (editableRect.width / 2))) {
+        isSnappedLeft = true;
+    } else {
+        isSnappedRight = true;
+    }
    resizing = false;
-   // do something with the end of the resize event
+   draggable = false;
 });
