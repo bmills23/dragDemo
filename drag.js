1d72374cba53936431998f0da4b89daaa84e05aa
Bryan Mills <bryan.mills@palmetto.lan>
2023-01-18 16:22:42 -0700

    No progress :(

--- drag.js
+++ drag.js
@@ -62,13 +62,18 @@ window.addEventListener("resize", () => {
 })
 
 class Image {
-    constructor(resizing = false, aspectRatio = 0, initialX = 0, initialY = 0, initialWidth = 0, initialHeight = 0, //Resizing
+    constructor(image, //Image
+        
+                resizing = false, aspectRatio = 0, initialX = 0, initialY = 0, initialWidth = 0, initialHeight = 0, //Resizing
 
                 draggable = false, offsetX = 0, offsetY = 0, isSnappedLeft = false, isSnappedRight = false, isPlaced = false, //Dragging
  
-                marginResizing = false, marginLeft = 0, marginRight = 0, marginTop = 0, marginBottom = 0) //Margin Editing) 
+                marginResizing = false, marginLeft = 0, marginRight = 0, marginTop = 0, marginBottom = 0, //Margin Editing) 
     
+                eventAdded = true) //Event Listening, defaults to true due forEach loop (line 377ish)
     { 
+
+        this.image = image
 
         this.resizing = resizing;
         this.aspectRatio = aspectRatio;
@@ -89,37 +94,15 @@ class Image {
         this.marginRight = marginRight;
         this.marginTop = marginTop;
         this.marginBottom = marginBottom;
+
+        this.eventAdded = eventAdded
+
     }
 
-}
+    //Arrow Functions Inherit 'this' From Surrounding Scope
 
-//Each Image Needs It's Own Event Listener
-images.forEach((image,index) => {
-
-    //Instatiate New Object for Each Image 
-    let imageObject = new Image()
-
-    imageObjects.push(imageObject)
-
-    image.addEventListener("mousedown", event => {
-
-        let handleMouseDownFunc = handleMouseDown.bind(imageObjects[index])
-        let handleMouseDownDivFunc = handleMouseDownDiv.bind(imageObjects[index])
-        let handleMouseMoveFunc = handleMouseMove.bind(imageObjects[index])
-    
-        if (imageObjects[index].eventAdded) {
-            image.removeEventListener("mousedown", handleMouseDownFunc);
-            editable.removeEventListener("mousedown", handleMouseDownDivFunc);
-            editable.removeEventListener("mousemove", handleMouseMoveFunc);
-        } else {
-            image.addEventListener("mousedown", handleMouseDownFunc);
-            editable.addEventListener("mousedown", handleMouseDownDivFunc);
-            editable.addEventListener("mousemove", handleMouseMoveFunc);
-        } 
-        
-    })
-
-    function handleMouseDown(event) {
+    //Handle Mouse Down Function
+    handleMouseDown = (event) => {
 
         if (!this.resizing && 
             !this.draggable && 
@@ -127,14 +110,16 @@ images.forEach((image,index) => {
         
               //Dropdown Activated
               dropdown.classList.remove("hidden");
+
+              console.log("Dropdown unhidden")
          
-              dropdown.style.top = image.offsetTop + image.offsetHeight + "px"
-              dropdown.style.left = image.offsetLeft + image.offsetWidth + "px"
+              dropdown.style.top = this.image.offsetTop + this.image.offsetHeight + "px"
+              dropdown.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
         
               // Add event listeners to the buttons
-              resizeButton.addEventListener("click", handleResize.bind(this));
-              moveButton.addEventListener("click", handleMove.bind(this));
-              marginButton.addEventListener("click", handleMargin.bind(this));
+              resizeButton.addEventListener("click", event => this.handleResize(event));
+              moveButton.addEventListener("click",event => this.handleMove(event));
+              marginButton.addEventListener("click", event => this.handleMargin(event));
       
             } else {
     
@@ -147,11 +132,7 @@ images.forEach((image,index) => {
             this.marginRight = false;
             this.marginTop = false;
             this.marginBottom = false;
-
-            resizeButton.removeEventListener("click", handleResize.bind(this));
-            moveButton.removeEventListener("click", handleMove.bind(this));
-            marginButton.removeEventListener("click", handleMargin.bind(this));
-        
+       
             dropdown.classList.add("hidden");
             marginDropdown.classList.add("hidden");
        
@@ -172,7 +153,7 @@ images.forEach((image,index) => {
     }
     
     //If mousedown occurs while resizing/dragging within parent, cancels the action
-    function handleMouseDownDiv(event) {
+    handleMouseDownDiv = (event) => {
         
         if (this.resizing || this.draggable) {
         
@@ -200,34 +181,36 @@ images.forEach((image,index) => {
     
     }
       
-    function handleResize(event) {
-    
+    handleResize = (event) => {
+
         // Set up resizing variables
         this.resizing = true;   
         this.draggable = false;
         this.marginResizing = false;
-        this.aspectRatio = image.clientWidth / image.clientHeight;
+        this.aspectRatio = this.image.clientWidth / this.image.clientHeight;
         this.initialX = event.clientX;
         this.initialY = event.clientY;
-        this.initialWidth = image.clientWidth;
-        this.initialHeight = image.clientHeight;
+
+        console.log(event.clientX, event.clientY)
+        this.initialWidth = this.image.clientWidth;
+        this.initialHeight = this.image.clientHeight;
         dropdown.classList.add("hidden");
     
     }
       
-    function handleMove(event) {
+   handleMove = (event) => {
     
         // Set up draggable variables
         this.draggable = true;
         this.resizing = false;
         this.marginResizing = false;
-        this.offsetX = event.clientX - image.offsetLeft;
-        this.offsetY = event.clientY - image.offsetTop;
+        this.offsetX = event.clientX - this.image.offsetLeft;
+        this.offsetY = event.clientY - this.image.offsetTop;
         dropdown.classList.add("hidden");
         
     }
     
-    function handleMargin (event) {
+    handleMargin = (event) => { 
             
         this.marginResizing = true;
         this.draggable = false;
@@ -236,33 +219,33 @@ images.forEach((image,index) => {
         marginDropdown.classList.remove("hidden")
         dropdown.classList.add("hidden")
 
-        marginDropdown.style.top = image.offsetTop + image.offsetHeight + "px"
-        marginDropdown.style.left = image.offsetLeft + image.offsetWidth + "px"
+        marginDropdown.style.top = this.image.offsetTop + this.image.offsetHeight + "px"
+        marginDropdown.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
 
-        marginSliderRight.style.top = image.offsetTop + image.offsetHeight + "px" 
-        marginSliderRight.style.left = image.offsetLeft + image.offsetWidth + "px"
+        marginSliderRight.style.top = this.image.offsetTop + this.image.offsetHeight + "px" 
+        marginSliderRight.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
 
-        marginSliderTop.style.top = image.offsetTop + image.offsetHeight + "px" 
-        marginSliderTop.style.left = image.offsetLeft + image.offsetWidth + "px"
+        marginSliderTop.style.top = this.image.offsetTop + this.image.offsetHeight + "px" 
+        marginSliderTop.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
 
-        marginSliderBottom.style.top = image.offsetTop + image.offsetHeight + "px" 
-        marginSliderBottom.style.left = image.offsetLeft + image.offsetWidth + "px"
-    
+        marginSliderBottom.style.top = this.image.offsetTop + this.image.offsetHeight + "px" 
+        marginSliderBottom.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
+
     }
     
-    function handleMouseMove(event) {
+    handleMouseMove = (event) => {
           
         if (this.resizing) {
         
-            let imageRect = image.getBoundingClientRect()
+            let imageRect = this.image.getBoundingClientRect()
               
-            let left = event.clientX - imageObjects[index].offsetX;
-            let top = event.clientY - imageObjects[index].offsetY;
-            image.style.left = left + "px";
-            image.style.top = top + "px";
+            let left = event.clientX - this.offsetX;
+            let top = event.clientY - this.offsetY;
+            this.image.style.left = left + "px";
+            this.image.style.top = top + "px";
 
-            let newWidth = this.aspectRatio * (event.clientY - imageObjects[index].initialY + imageObjects[index].initialHeight);
-            let newHeight = event.clientY - imageObjects[index].initialY + imageObjects[index].initialHeight;
+            let newWidth = this.aspectRatio * (event.clientY - this.initialY + this.initialHeight);
+            let newHeight = event.clientY - this.initialY + this.initialHeight;
 
             if(newWidth > editableRect.width) {
                 newWidth = editableRect.width 
@@ -271,19 +254,19 @@ images.forEach((image,index) => {
                 newHeight = editableRect.height;
             }
 
-            image.style.width = newWidth + "px";
+            this.image.style.width = newWidth + "px";
         
             if (imageRect.left < editableRect.left) {
-                image.style.left = editableRect.left;
+                this.image.style.left = editableRect.left;
             }
             if (imageRect.top < editableRect.top) {
-                image.style.top = editableRect.top;
+                this.image.style.top = editableRect.top;
             }
             if (imageRect.right > editableRect.right) {
-                image.style.left = editableRect.right - imageRect.width + "px";
+                this.image.style.left = editableRect.right - imageRect.width + "px";
             } 
             if (imageRect.bottom > editableRect.bottom) {
-                image.style.top = editableRect.bottom - imageRect.height + "px";
+                this.image.style.top = editableRect.bottom - imageRect.height + "px";
             }   
           
         if (this.draggable) {
@@ -298,7 +281,7 @@ images.forEach((image,index) => {
     
             // Check if cursor is within left or right half of parent div
             if (event.clientX < middleX) {
-                image.style.cssFloat = "left";
+                this.image.style.cssFloat = "left";
                 this.isSnappedLeft = true;
                 this.isSnappedRight = false;
             } else if (event.clientX >= middleX) {
@@ -330,8 +313,8 @@ images.forEach((image,index) => {
                 marginSliderLeft.style.top = image.offsetTop + image.offsetHeight + "px" 
                 marginSliderLeft.style.left = image.offsetLeft + image.offsetWidth + "px"
         
-                marginSliderLeft.addEventListener("input", function () {
-                    image.style.marginLeft = this.value + "px";
+                marginSliderLeft.addEventListener("input", () => {
+                    this.image.style.marginLeft = marginSliderLeft.value + "px";
                 });
         
             })
@@ -343,11 +326,11 @@ images.forEach((image,index) => {
                 marginDropdown.classList.add("hidden")
                 marginSliderRight.classList.remove("hidden")
         
-                marginSliderRight.style.top = image.offsetTop + image.offsetHeight + "px" 
-                marginSliderRight.style.left = image.offsetLeft + image.offsetWidth + "px"
+                marginSliderRight.style.top = this.image.offsetTop + this.image.offsetHeight + "px" 
+                marginSliderRight.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
         
-                marginSliderRight.addEventListener("input", function () {
-                    image.style.marginRight = this.value + "px";
+                marginSliderRight.addEventListener("input", () => {
+                    this.image.style.marginRight = marginSliderRight.value + "px";
                 });
         
         
@@ -360,11 +343,11 @@ images.forEach((image,index) => {
                 marginDropdown.classList.add("hidden")
                 marginSliderTop.classList.remove("hidden")
         
-                marginSliderTop.style.top = image.offsetTop + image.offsetHeight + "px" 
-                marginSliderTop.style.left = image.offsetLeft + image.offsetWidth + "px"
+                marginSliderTop.style.top = this.image.offsetTop + this.image.offsetHeight + "px" 
+                marginSliderTop.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
         
-                marginSliderTop.addEventListener("input", function () {
-                    image.style.marginTop = this.value + "px";
+                marginSliderTop.addEventListener("input", () => {
+                    this.image.style.marginTop = marginSliderTop.value + "px";
                 });
         
             })
@@ -376,11 +359,11 @@ images.forEach((image,index) => {
                 marginDropdown.classList.add("hidden")
                 marginSliderBottom.classList.remove("hidden")
         
-                marginSliderBottom.style.top = image.offsetTop + image.offsetHeight + "px" 
-                marginSliderBottom.style.left = image.offsetLeft + image.offsetWidth + "px"
+                marginSliderBottom.style.top = this.image.offsetTop + this.image.offsetHeight + "px" 
+                marginSliderBottom.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
         
-                marginSliderBottom.addEventListener("input", function () {
-                    image.style.marginBottom = this.value + "px";
+                marginSliderBottom.addEventListener("input", () => {
+                    this.image.style.marginBottom = marginSliderBottom.value + "px";
                 });
         
             })
@@ -390,8 +373,46 @@ images.forEach((image,index) => {
         }
             
     }
+
+
+    toggleEventListeners = (event) => {
+
+        if (this.eventAdded) {
+
+            this.image.addEventListener("mousedown", this.handleMouseDown);
+            editable.addEventListener("mousedown", this.handleMouseDownDiv);
+            editable.addEventListener("mousemove", this.handleMouseMove);
+
+            this.eventAdded = false;
+
+       } else {
+
+            this.image.removeEventListener("mousedown", this.handleMouseDown);
+            editable.removeEventListener("mousedown", this.handleMouseDownDiv);
+            editable.removeEventListener("mousemove", this.handleMouseMove);
+            
+            this.eventAdded = true;
+
+       }
+    }
         
-    })
+}
+
+//Each Image Needs It's Own Event Listener
+images.forEach((image) => {
+
+    //Instatiate New Object for Each Image 
+    let imageObject = new Image(image)
+
+    //For Logging
+    imageObjects.push(imageObject)  
+
+    imageObject.toggleEventListeners()
+
+    
+
+
+})
 
 
 //Coordinates Appended to Move Button
