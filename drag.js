bc6e17fb9177466aae8bded73abdbb5f543512f0
bmills23 <58231400+bmills23@users.noreply.github.com>
2023-01-28 23:41:35 -0700

    Update drag.js

--- drag.js
+++ drag.js
@@ -38,16 +38,15 @@ window.addEventListener("resize", () => {
 })
 
 class Image {
-    constructor(image, //Image
+    constructor(image, //Image DOM Element
         
                 resizing = false, aspectRatio = 0, initialX = 0, initialY = 0, initialWidth = 0, initialHeight = 0, //Resizing
 
                 draggable = false, offsetX = 0, offsetY = 0, isSnappedLeft = false, isSnappedRight = false, isPlaced = false, //Dragging
  
-                marginResizing = false, marginLeft = 0, marginRight = 0, marginTop = 0, marginBottom = 0, //Margin Editing) 
-    
-                eventAdded = false) //Event Listening, defaults to false, needs to be activated with click
-    { 
+                marginResizing = false, marginLeft = 0, marginRight = 0, marginTop = 0, marginBottom = 0) //Margin Editing 
+
+    {
 
         this.image = image
 
@@ -71,13 +70,80 @@ class Image {
         this.marginTop = marginTop;
         this.marginBottom = marginBottom;
 
-        this.eventAdded = eventAdded
-
+        //Mouse Move Handler for Event Listening Matchup
         this.handleMMRef = event => this.handleMouseMove(event)
+      
+        //Handlers for Margin Editing
+
+        //Left
+        this.leftMarginClickHandler = (event) => {
+            this.marginLeft = true;
+            marginDropdown.classList.add("hidden");
+            marginSliderLeft.classList.remove("hidden");
+          
+            marginSliderLeft.style.top = this.image.offsetTop + this.image.offsetHeight + "px";
+            marginSliderLeft.style.left = this.image.offsetLeft + this.image.offsetWidth + "px";
+          
+            marginSliderLeft.addEventListener("input", this.inputHandlerLeft)
+        }
+
+        this.inputHandlerLeft = () => {
+            this.image.style.marginLeft = marginSliderLeft.value + "px";
+        }
+
+        //Right
+        this.rightMarginClickHandler = (event) => {
+            this.marginRight = true;
+            marginDropdown.classList.add("hidden");
+            marginSliderRight.classList.remove("hidden");
+          
+            marginSliderRight.style.top = this.image.offsetTop + this.image.offsetHeight + "px";
+            marginSliderRight.style.left = this.image.offsetLeft + this.image.offsetWidth + "px";
+          
+            marginSliderRight.addEventListener("input", this.inputHandlerRight)
+        }
+
+        this.inputHandlerRight = () => {
+            this.image.style.marginRight = marginSliderRight.value + "px";
+        }
+
+        //Top
+        this.topMarginClickHandler = (event) => {
+            this.marginTop = true;
+            marginDropdown.classList.add("hidden");
+            marginSliderTop.classList.remove("hidden");
+          
+            marginSliderTop.style.top = this.image.offsetTop + this.image.offsetHeight + "px";
+            marginSliderTop.style.left = this.image.offsetLeft + this.image.offsetWidth + "px";
+          
+            marginSliderTop.addEventListener("input", this.inputHandlerTop)
+        }
+
+        this.inputHandlerTop = () => {
+            this.image.style.marginTop = marginSliderTop.value + "px";
+        }
+
+        //Bottom
+        this.bottomMarginClickHandler = (event) => {
+            this.marginBottom = true;
+            marginDropdown.classList.add("hidden");
+            marginSliderBottom.classList.remove("hidden");
+          
+            marginSliderBottom.style.top = this.image.offsetTop + this.image.offsetHeight + "px";
+            marginSliderBottom.style.left = this.image.offsetLeft + this.image.offsetWidth + "px";
+          
+            marginSliderBottom.addEventListener("input", this.handleMarginButton) 
+        }
+
+        this.inputHandlerBottom = () => {
+            this.image.style.marginBottom = marginSliderBottom.value + "px";
+        }
 
     }
 
-    //Arrow Functions Inherit 'this' From Surrounding Scope
+
+
+    //Arrow Functions Inherit 'this' From Parent Scope
 
     //Handle Mouse Down Function
     handleMouseDown = (event) => {
@@ -105,22 +171,31 @@ class Image {
       
             } else {
 
-            if (!this.marginLeft || 
+            //Reset all Boolean Values
+            if (this.resizing || this.draggable || this.marginResizing) {
+            
+                this.resizing = false;
+                this.draggable = false;
+                
+                this.marginResizing = false;
+            
+                this.marginLeft = false;
+                this.marginRight = false;
+                this.marginTop = false;
+                this.marginBottom = false;
+                
+            } else if (!this.marginLeft ||
                 !this.marginRight || 
                 !this.marginTop || 
                 !this.marginBottom) {
-
-                this.resizing = false;
-                this.draggable = false;
                     
-                    this.marginResizing = false;
-               
                 marginSliderLeft.classList.add("hidden")
                 marginSliderRight.classList.add("hidden")
                 marginSliderTop.classList.add("hidden")
                 marginSliderBottom.classList.add("hidden")
-        
+                            
             }
+    
               
         }
     
@@ -128,8 +203,19 @@ class Image {
     
     //If mousedown occurs while resizing/dragging within parent, cancels the action
     handleMouseDownDiv = (event) => {
+
+        leftMargin.removeEventListener("click", this.leftMarginClickHandler)
+        rightMargin.removeEventListener("click", this.rightMarginClickHandler)
+        topMargin.removeEventListener("click", this.topMarginClickHandler)
+        bottomMargin.removeEventListener("click", this.bottomMarginClickHandler)
+
+        marginSliderLeft.removeEventListener("input", this.inputHandlerLeft)
+        marginSliderRight.removeEventListener("input", this.inputHandlerRight)
+        marginSliderTop.removeEventListener("input", this.inputHandlerTop)
+        marginSliderBottom.removeEventListener("input", this.inputHandlerBottom)
         
-        if (this.resizing || this.draggable) {
+        //Reset all Boolean Values
+        if (this.resizing || this.draggable || this.marginResizing) {
         
             this.resizing = false;
             this.draggable = false;
@@ -157,8 +243,6 @@ class Image {
       
     handleResize = (event) => {
 
-        console.log("Resizing")
-
         editable.addEventListener("mousedown", this.removeMouseMove, { once : true });
         this.image.addEventListener("mousedown", this.removeMouseMove, { once : true });
         
@@ -170,15 +254,22 @@ class Image {
         this.initialX = event.clientX;
         this.initialY = event.clientY;
 
-        console.log(event.clientX, event.clientY)
         this.initialWidth = this.image.clientWidth;
         this.initialHeight = this.image.clientHeight;
         dropdown.classList.add("hidden");
 
     }
-      
-   handleMove = (event) => {
-        console.log("Moving")    
+
+    //Removes Mouse Move in handleMove method while creating new listeners for mousedown
+    //The handleMMRef is essential for making sure that the added and subtracted listeners are equal
+
+    removeMouseMove = () => {
+
+        editable.removeEventListener("mousemove", this.handleMMRef);
+
+    }
+
+   handleMove = (event) => {  
 
         editable.addEventListener("mousedown", this.removeMouseMove, { once : true });
         this.image.addEventListener("mousedown", this.removeMouseMove, { once : true });
@@ -193,17 +284,16 @@ class Image {
         
     }
 
-    removeMouseMove = () => {
-
-        editable.removeEventListener("mousemove", this.handleMMRef);
-
-    }
-    
     handleMargin = (event) => { 
-            
+
         this.marginResizing = true;
         this.draggable = false;
         this.resizing = false;
+
+        this.marginLeft = false;
+        this.marginRight = false;
+        this.marginTop = false;
+        this.marginBottom = false;
 
         marginDropdown.classList.remove("hidden")
         dropdown.classList.add("hidden")
@@ -227,8 +317,6 @@ class Image {
         if (this.resizing) {
         
             let imageRect = this.image.getBoundingClientRect()
-
-            console.log(imageRect)
               
             let left = event.clientX - this.offsetX;
             let top = event.clientY - this.offsetY;
@@ -264,8 +352,6 @@ class Image {
           
         if (this.draggable) {
 
-            console.log(this.draggable)
-                  
             event.target.style.cursor = "pointer";
 
             let left = event.clientX - this.offsetX;
@@ -286,98 +372,42 @@ class Image {
             }
     
             for (let i = 0; i < paragraphs.length; i++) {
-                let rect = paragraphs[i].getBoundingClientRect()
-                let lowerQuarter = rect.bottom / 4 
-                if (event.clientX > rect.left && event.clientX < rect.right && event.clientY > rect.bottom - lowerQuarter && event.clientY < rect.bottom) {
+                let rect = paragraphs[i].getBoundingClientRect();
+
+                let top = rect.top;
+                let bottom = rect.bottom;
+              
+                if (event.clientY > top && event.clientY < bottom) {
                     paragraphs[i].appendChild(this.image);
+                    break;
                 }
-                    
-            }
+
+              }
             
         }
         
         if (this.marginResizing) {
-
-            console.log(this.marginResizing)
               
-            leftMargin.addEventListener("click", event => {
-    
-                this.marginLeft = true;
-            
-                marginDropdown.classList.add("hidden")
-                marginSliderLeft.classList.remove("hidden")
-        
-                marginSliderLeft.style.top = this.image.offsetTop + this.image.offsetHeight + "px" 
-                marginSliderLeft.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
-        
-                marginSliderLeft.addEventListener("input", () => {
-                    this.image.style.marginLeft = marginSliderLeft.value + "px";
-                });
-        
-            })
+            leftMargin.addEventListener("click", this.leftMarginClickHandler)
 
-            rightMargin.addEventListener("click", event => {
-        
-                this.marginRight = true;
-        
-                marginDropdown.classList.add("hidden")
-                marginSliderRight.classList.remove("hidden")
-        
-                marginSliderRight.style.top = this.image.offsetTop + this.image.offsetHeight + "px" 
-                marginSliderRight.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
-        
-                marginSliderRight.addEventListener("input", () => {
-                    this.image.style.marginRight = marginSliderRight.value + "px";
-                });
-        
-        
-            })
+            rightMargin.addEventListener("click", this.rightMarginClickHandler)
 
-            topMargin.addEventListener("click", event => {
-        
-                this.marginTop = true;
-        
-                marginDropdown.classList.add("hidden")
-                marginSliderTop.classList.remove("hidden")
-        
-                marginSliderTop.style.top = this.image.offsetTop + this.image.offsetHeight + "px" 
-                marginSliderTop.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
-        
-                marginSliderTop.addEventListener("input", () => {
-                    this.image.style.marginTop = marginSliderTop.value + "px";
-                });
-        
-            })
+            topMargin.addEventListener("click", this.topMarginClickHandler)
 
-            bottomMargin.addEventListener("click", event => {
-    
-                this.marginBottom = true;
-        
-                marginDropdown.classList.add("hidden")
-                marginSliderBottom.classList.remove("hidden")
-        
-                marginSliderBottom.style.top = this.image.offsetTop + this.image.offsetHeight + "px" 
-                marginSliderBottom.style.left = this.image.offsetLeft + this.image.offsetWidth + "px"
-        
-                marginSliderBottom.addEventListener("input", () => {
-                    this.image.style.marginBottom = marginSliderBottom.value + "px";
-                });
-        
-            })
-       
-            }
-    
+            bottomMargin.addEventListener("click", this.bottomMarginClickHandler)
+
         }
             
+    }
+
+    
 }
 
-        
 
+images.forEach((image) => {
 
-let selectedImageIndex = -1
-
-images.forEach((image, index) => {
     let imageObject = new Image(image)
+
     imageObjects.push(imageObject)
 
     image.addEventListener("click", event => {
@@ -401,7 +431,6 @@ moveButton.appendChild(coordinates);
 document.addEventListener("mousemove", event => {
     coordinates.innerHTML = `X: ${event.clientX}, Y: ${event.clientY}`;
 });
-
 
 
 
